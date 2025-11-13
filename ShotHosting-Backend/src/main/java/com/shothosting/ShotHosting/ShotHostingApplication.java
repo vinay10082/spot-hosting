624eba87bcabcfaf56b8c03e99750660c74fecb6
vinay10082 <vinay06102001@gmail.com>
2025-11-14 00:20:36 +0530

    backend initial setup is done

---
+++ ShotHosting-Backend/src/main/java/com/shothosting/ShotHosting/ShotHostingApplication.java
@@ -0,0 +1,13 @@
+package com.shothosting.ShotHosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShotHostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShotHostingApplication.class, args);
+	}
+
+}
